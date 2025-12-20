e93d7dd6173a4add5d2fa55a84b5508fca5c5ce2
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-19 17:01:30 -0800

    Updates for OpenVox

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject puppetlabs/trapperkeeper-scheduler "1.2.4-SNAPSHOT"
+(defproject org.openvoxproject/trapperkeeper-scheduler "1.2.4-SNAPSHOT"
   :description "Trapperkeeper Scheduler Service"
 
   :dependencies [[org.clojure/clojure]
-                 [puppetlabs/trapperkeeper]
-                 [puppetlabs/i18n]
-                 [puppetlabs/kitchensink]
+                 [org.openvoxproject/trapperkeeper]
+                 [org.openvoxproject/i18n]
+                 [org.openvoxproject/kitchensink]
                  [org.quartz-scheduler/quartz "2.3.2" :exclusions [c3p0]]]
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "7.3.30"]
+  :parent-project {:coords [org.openvoxproject/clj-parent "7.4.1-SNAPSHOT"]
                    :inherit [:managed-dependencies]}
 
   :pedantic? :abort
@@ -28,10 +28,10 @@
                                      :sign-releases false}]]
 
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[puppetlabs/trapperkeeper :classifier "test" :scope "test"]
-                                  [puppetlabs/kitchensink :classifier "test" :scope "test"]]}}
+                   :dependencies [[org.openvoxproject/trapperkeeper :classifier "test" :scope "test"]
+                                  [org.openvoxproject/kitchensink :classifier "test" :scope "test"]]}}
 
   :plugins  [[lein-parent "0.3.7"]
-             [puppetlabs/i18n "0.8.0"]]
+             [org.openvoxproject/i18n "0.8.0"]]
   :aot [puppetlabs.trapperkeeper.services.scheduler.job]
   :repl-options {:init-ns user})
