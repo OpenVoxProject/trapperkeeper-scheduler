15b386797d41e7893fc1bb89e16846f4a434017e
nmburgan <13688219+nmburgan@users.noreply.github.com>
2026-01-02 00:46:21 -0800

    Update clj-parent, lein-parent, i18n

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/trapperkeeper-scheduler "1.2.5-SNAPSHOT"
+(defproject org.openvoxproject/trapperkeeper-scheduler "1.3.0-SNAPSHOT"
   :description "Trapperkeeper Scheduler Service"
 
   :dependencies [[org.clojure/clojure]
@@ -9,7 +9,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [org.openvoxproject/clj-parent "7.5.1"]
+  :parent-project {:coords [org.openvoxproject/clj-parent "7.6.3"]
                    :inherit [:managed-dependencies]}
 
   :pedantic? :abort
@@ -31,7 +31,7 @@
                    :dependencies [[org.openvoxproject/trapperkeeper :classifier "test" :scope "test"]
                                   [org.openvoxproject/kitchensink :classifier "test" :scope "test"]]}}
 
-  :plugins  [[lein-parent "0.3.7"]
-             [org.openvoxproject/i18n "0.9.4"]]
+  :plugins  [[lein-parent "0.3.9"]
+             [org.openvoxproject/i18n "1.0.2"]]
   :aot [puppetlabs.trapperkeeper.services.scheduler.job]
   :repl-options {:init-ns user})
