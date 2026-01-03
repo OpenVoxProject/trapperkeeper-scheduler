1cb5224803b31e435df6cf4eb793a72810a59f77
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-01-03 04:32:55 +0000

    Version 1.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/trapperkeeper-scheduler "1.3.0"
+(defproject org.openvoxproject/trapperkeeper-scheduler "1.3.1-SNAPSHOT"
   :description "Trapperkeeper Scheduler Service"
 
   :dependencies [[org.clojure/clojure]
