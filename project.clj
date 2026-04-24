c6a45cd23f28c5582f66959db185201cc10ef899
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-04-24 11:40:09 +0000

    Version 1.3.2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def i18n-version "1.0.4")
 
-(defproject org.openvoxproject/trapperkeeper-scheduler "1.3.2-SNAPSHOT"
+(defproject org.openvoxproject/trapperkeeper-scheduler "1.3.2"
   :description "Trapperkeeper Scheduler Service"
 
   ;; Generally, try to keep version pins in :managed-dependencies and the libraries
